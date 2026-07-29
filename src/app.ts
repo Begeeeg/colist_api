fa3c122943e11bd1b890f2c1769c3a21761cc6ea
Begeeeg <v.yecyec7@gmail.com>
2026-07-29 09:26:35 +0800

    ♻ ref(app): adding imports

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,18 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "http://localhost:3000",
+        credentials: true,
+    })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 export default app;
